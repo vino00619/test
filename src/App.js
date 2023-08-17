6d6d1ce5e67cd2425d84078a6e6929125cf6fb12
vino00619 <vinoth00619@gmail.com>
2023-08-17 23:46:15 +0530

    final commit of table task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,148 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import data from "./data";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { useState } from "react";
+
+
+const TabCol = Object.keys(data[0]);
+// console.log(TabCol);
+
+const TabHead = () => {
+  return TabCol.map((data, index) => {
+    //console.log(data);
+    return <th key={index}>{data}</th>;
+  });
+};
+
+const TabData = () => {
+  return data.map((data) => {
+    return (
+      <tr>
+        {TabCol.map((i) => {
+          return <td>{data[i]}</td>;
+        })}
+      </tr>
+    );
+  });
+};
+
+const uniqueCity = [...new Set(data.map((item) => item.City))];
+console.log("unique city", uniqueCity);
+
+const uniqueDistrict = [...new Set(data.map((item) => item.District))];
+console.log("unique city", uniqueCity);
+
 function App() {
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [selectedDistrict, setSelectedDistrict] = useState(null);
+  const [street, setStreet] = useState([]);
+
+ 
+
+  function ButtonClick() {
+    const filteredData = data
+      .filter((d) => {
+      if(d.City === selectedCity){console.log("city", d)};
+        return d.City.trim() === selectedCity.trim();
+      })
+      .filter((d) => {
+      if(d.District === selectedDistrict){console.log("dist", d)};
+        return d.District.trim() === selectedDistrict.trim();
+      })
+      .map((d) => d.Street);
+
+      console.log(filteredData);
+
+      const uniqueFilteredData = [...new Set(filteredData)];
+      setStreet(uniqueFilteredData)
+      // console.log(uniqueFilteredData);
+  }
+  
+
   return (
     <div>
-      Hello
+      <table className="table">
+        <thead>
+          <tr>{TabHead()}</tr>
+        </thead>
+        <tbody>{TabData()}</tbody>
+      </table>
+
+      <div class="btn-group">
+        <button
+          type="button"
+          class="btn btn-primary dropdown-toggle"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {selectedCity ? selectedCity : "Select City"}
+        </button>
+        <ul
+          class="dropdown-menu"
+          onClick={(e) => {
+            setSelectedCity(e.target.innerText)
+            e.preventDefault()
+            }}
+        >
+          {uniqueCity.map((d) => {
+            return (
+              <div>
+                {" "}
+                <li>
+                  <a class="dropdown-item" href="#">
+                    {d}
+                  </a>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div class="btn-group">
+        <button
+          type="button"
+          class="btn btn-primary dropdown-toggle"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {selectedDistrict ? selectedDistrict : "Selected District"}
+        </button>
+        <ul
+          class="dropdown-menu"
+          onClick={(e) => {
+            setSelectedDistrict(e.target.innerText)
+            e.preventDefault()
+            }}
+        >
+          {uniqueDistrict.map((d) => {
+            return (
+              <div>
+                {" "}
+                <li>
+                  <a class="dropdown-item" href="#">
+                    {d}
+                  </a>
+                </li>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+
+      <button type="button" class="btn btn-success" onClick={ButtonClick}>
+        Success
+      </button>
+
+      {street.map((d) => {
+            return (
+              <p>
+                {d}
+              </p>
+            );
+          })}
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,374 @@
+const data = [
+    {
+      "Street Code": 49970,
+      "Street": "JLN DUYONG",
+      "District No.": 3,
+      "District": "BUKIT DUYONG",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 221954,
+      "Street": "SLK BALIK HUTAN",
+      "District No.": 4,
+      "District": "BUKIT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 222249,
+      "Street": "SLK BUKIT BAYAN",
+      "District No.": 4,
+      "District": "BUKIT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 44113,
+      "Street": "JLN BUKIT KATIL",
+      "District No.": 4,
+      "District": "BUKIT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 222249,
+      "Street": "SLK BUKIT BAYAN",
+      "District No.": 4,
+      "District": "BUKIT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 72043,
+      "Street": "JLN KNMP 2A",
+      "District No.": 4,
+      "District": "BUKIT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 222249,
+      "Street": "SLK BUKIT BAYAN",
+      "District No.": 4,
+      "District": "BUKIT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 61468,
+      "Street": "JLN IKS BK 2",
+      "District No.": 6,
+      "District": "IND KECIL SEDERHANA BKT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 222711,
+      "Street": "SLK KG TELOK JLN BUKIT BERUANG",
+      "District No.": 13,
+      "District": "KG TELOK, BKT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 40887,
+      "Street": "JLN BKT KATIL",
+      "District No.": 14,
+      "District": "KG TUN RAZAK, HANG TUAH JAYA",
+      "Postl Code": 75450,
+      "City Code": 819,
+      "City": "AYER KEROH",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 77623,
+      "Street": "JLN KF 1",
+      "District No.": 17,
+      "District": "KOTA FESYEN MITC",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 77623,
+      "Street": "JLN KF 1",
+      "District No.": 17,
+      "District": "KOTA FESYEN MITC",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 77623,
+      "Street": "JLN KF 1",
+      "District No.": 17,
+      "District": "KOTA FESYEN MITC",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 77623,
+      "Street": "JLN KF 1",
+      "District No.": 17,
+      "District": "KOTA FESYEN MITC",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 107121,
+      "Street": "JLN OZ 30",
+      "District No.": 22,
+      "District": "OZANA VILLAS, BKT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 107102,
+      "Street": "JLN OZ 29",
+      "District No.": 22,
+      "District": "OZANA VILLAS, BKT KATIL ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 62675,
+      "Street": "JLN GAPAM",
+      "District No.": 27,
+      "District": "TIARA GOLF & COUNTRY CLUB ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 18651,
+      "Street": "JLN BBI 1",
+      "District No.": 39,
+      "District": "TMN BUKIT BERUANG INDAH ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 33574,
+      "Street": "JLN BKP 2",
+      "District No.": 51,
+      "District": "TMN BUKIT KATIL PERMAI",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 33652,
+      "Street": "JLN BKP 4",
+      "District No.": 51,
+      "District": "TMN BUKIT KATIL PERMAI",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 33652,
+      "Street": "JLN BKP 4",
+      "District No.": 51,
+      "District": "TMN BUKIT KATIL PERMAI",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 33514,
+      "Street": "JLN BKP 1",
+      "District No.": 51,
+      "District": "TMN BUKIT KATIL PERMAI",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 104713,
+      "Street": "JLN PERAK",
+      "District No.": 65,
+      "District": "TMN MEGAH, BUKIT BERUANG",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 126770,
+      "Street": "JLN SERI RAMA",
+      "District No.": 73,
+      "District": "TMN MUZAFFAR SHAH ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 137457,
+      "Street": "JLN SI 18",
+      "District No.": 81,
+      "District": "TMN SAUJANA INDAH ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 137457,
+      "Street": "JLN SI 18",
+      "District No.": 81,
+      "District": "TMN SAUJANA INDAH ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 41026,
+      "Street": "JLN BKT KATIL",
+      "District No.": 90,
+      "District": "TMN SURIA, HANG TUAH JAYA ",
+      "Postl Code": 75450,
+      "City Code": 820,
+      "City": "Alor Gajah",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 170465,
+      "Street": "JLN TU 2",
+      "District No.": 91,
+      "District": "TMN TASIK UTAMA, HANG TUAH JAYA ",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 171634,
+      "Street": "JLN TU 9C",
+      "District No.": 91,
+      "District": "TMN TASIK UTAMA, HANG TUAH JAYA ",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 171684,
+      "Street": "JLN TU 9E",
+      "District No.": 91,
+      "District": "TMN TASIK UTAMA, HANG TUAH JAYA ",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 171087,
+      "Street": "JLN TU 42",
+      "District No.": 91,
+      "District": "TMN TASIK UTAMA, HANG TUAH JAYA ",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 170219,
+      "Street": "JLN TU 10",
+      "District No.": 91,
+      "District": "TMN TASIK UTAMA, HANG TUAH JAYA ",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 170244,
+      "Street": "JLN TU 10B",
+      "District No.": 91,
+      "District": "TMN TASIK UTAMA, HANG TUAH JAYA ",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 138225,
+      "Street": "JLN TK 3",
+      "District No.": 94,
+      "District": "TMN TUN KUDU",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 140053,
+      "Street": "JLN TK 6",
+      "District No.": 94,
+      "District": "TMN TUN KUDU",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 138225,
+      "Street": "JLN TK 3",
+      "District No.": 94,
+      "District": "TMN TUN KUDU",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    },
+    {
+      "Street Code": 138225,
+      "Street": "JLN TK 3",
+      "District No.": 94,
+      "District": "TMN TUN KUDU",
+      "Postl Code": 75450,
+      "City Code": 821,
+      "City": "Shah Alam",
+      "Rg": "MEL"
+    }
+   ]
+
+   export default data;
