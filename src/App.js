c3288e7369595d4d6b199e681770e0661bee51b7
vino00619 <vinoth00619@gmail.com>
2023-08-22 20:59:08 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const TabHead = () => {
   });
 };
 
-
 //mapping all the data into table data
 const TabData = () => {
   return data.map((data) => {
@@ -43,31 +42,44 @@ function App() {
   const [selectedDistrict, setSelectedDistrict] = useState(null);
   const [street, setStreet] = useState([]);
 
- 
-// filtering the data based on city and district and mapping its values
+  // filtering the data based on city and district and mapping its values
   function ButtonClick() {
     const filteredData = data
       .filter((d) => {
-      if(d.City === selectedCity){console.log("city", d)};
+        if (d.City === selectedCity) {
+          console.log("city", d);
+        }
         return d.City.trim() === selectedCity.trim();
       })
       .filter((d) => {
-      if(d.District === selectedDistrict){console.log("dist", d)};
+        if (d.District === selectedDistrict) {
+          console.log("dist", d);
+        }
         return d.District.trim() === selectedDistrict.trim();
       })
       .map((d) => d.Street);
 
-      console.log(filteredData);
+    console.log(filteredData);
 
-      // getting unique values in filtered data
-      const uniqueFilteredData = [...new Set(filteredData)];
-      setStreet(uniqueFilteredData)
-      // console.log(uniqueFilteredData);
+    // getting unique values in filtered data
+    const uniqueFilteredData = [...new Set(filteredData)];
+    setStreet(uniqueFilteredData);
+    // console.log(uniqueFilteredData);
   }
-  
 
   return (
     <div>
+      <nav
+        class="navbar bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1" style={{ fontSize: 50 }}>
+            Table
+          </span>
+        </div>
+      </nav>
+
       <table className="table">
         <thead>
           <tr>{TabHead()}</tr>
@@ -75,80 +87,90 @@ function App() {
         <tbody>{TabData()}</tbody>
       </table>
 
-      <div class="btn-group">
-        <button
-          type="button"
-          class="btn btn-primary dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          {selectedCity ? selectedCity : "Select City"}
-        </button>
-        <ul
-          class="dropdown-menu"
-          onClick={(e) => {
-            setSelectedCity(e.target.innerText)
-            e.preventDefault()
+      <div class="tableFunction">
+        <div class="btn-group">
+          <button
+            style={{ marginRight: "10px" }}
+            type="button"
+            class="btn btn-primary dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {selectedCity ? selectedCity : "Select City"}
+          </button>
+          <ul
+            class="dropdown-menu"
+            onClick={(e) => {
+              setSelectedCity(e.target.innerText);
+              e.preventDefault();
             }}
-        >
-          {uniqueCity.map((d) => {
-            return (
-              <div>
-                {" "}
-                <li>
-                  <a class="dropdown-item" href="#">
-                    {d}
-                  </a>
-                </li>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+          >
+            {uniqueCity.map((d) => {
+              return (
+                <div>
+                  {" "}
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      {d}
+                    </a>
+                  </li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
 
-      <div class="btn-group">
-        <button
-          type="button"
-          class="btn btn-primary dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          {selectedDistrict ? selectedDistrict : "Selected District"}
-        </button>
-        <ul
-          class="dropdown-menu"
-          onClick={(e) => {
-            setSelectedDistrict(e.target.innerText)
-            e.preventDefault()
+        <div class="btn-group">
+          <button
+            style={{ marginRight: "10px" }}
+            type="button"
+            class="btn btn-primary dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            {selectedDistrict ? selectedDistrict : "Selected District"}
+          </button>
+          <ul
+            class="dropdown-menu"
+            onClick={(e) => {
+              setSelectedDistrict(e.target.innerText);
+              e.preventDefault();
             }}
-        >
-          {uniqueDistrict.map((d) => {
-            return (
-              <div>
-                {" "}
-                <li>
-                  <a class="dropdown-item" href="#">
-                    {d}
-                  </a>
-                </li>
-              </div>
-            );
+          >
+            {uniqueDistrict.map((d) => {
+              return (
+                <div>
+                  {" "}
+                  <li>
+                    <a class="dropdown-item" href="#">
+                      {d}
+                    </a>
+                  </li>
+                </div>
+              );
+            })}
+          </ul>
+        </div>
+
+        <button type="button" class="btn btn-success" onClick={ButtonClick}>
+          Success
+        </button>
+        <div style={{ margin: "auto", paddingLeft: "10px" }}>
+          {street.map((d) => {
+            return <p>{d}</p>;
           })}
-        </ul>
+        </div>
       </div>
-
-      <button type="button" class="btn btn-success" onClick={ButtonClick}>
-        Success
-      </button>
-
-      {street.map((d) => {
-            return (
-              <p>
-                {d}
-              </p>
-            );
-          })}
-
+      <nav
+        class="navbar bg-dark border-bottom border-body"
+        data-bs-theme="dark"
+      >
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1" style={{ fontSize: 30 }}>
+            Footer
+          </span>
+        </div>
+      </nav>
     </div>
   );
 }
